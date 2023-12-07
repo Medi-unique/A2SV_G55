434aab1ba5c6d39040886de11530739999c819d2
Medina Nesro <mediunique00@gmail.com>
2023-12-07 11:34:00 +0300

    Added solution for Decompress Run-Length Encoded List

---
+++ leetcode_solutions/decompress-run-length-encoded-list.py
@@ -0,0 +1,12 @@
+class Solution:
+    def decompressRLElist(self, nums: List[int]) -> List[int]:
+        #evens are repeated odds value times
+        result=[]
+        for i,n in enumerate(nums):
+            if  i%2==0 and i+1 < len(nums):
+                for j in range(n):
+                    result.append(nums[i+1])
+            else:
+                i+=1
+        return result
+            
