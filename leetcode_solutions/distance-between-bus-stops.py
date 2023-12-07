d3ae316daad62bb3234f8642b33bb6c6ff0ae73b
Medina Nesro <mediunique00@gmail.com>
2023-12-07 08:51:44 +0300

    Added solution for Distance Between Bus Stops

---
+++ leetcode_solutions/distance-between-bus-stops.py
@@ -0,0 +1,7 @@
+class Solution:
+    def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
+        if start > destination:
+            start,destination=destination,start
+        return (min(sum(distance[start:destination]),sum(distance[destination:])+sum(distance[:start])))
+        
+        
