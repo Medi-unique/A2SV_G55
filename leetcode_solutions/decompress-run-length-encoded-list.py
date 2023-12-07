e1a053821ddaa5a09bc97314c4fc40c2cb2fe343
Medina Nesro <mediunique00@gmail.com>
2023-12-07 11:45:55 +0300

    Added solution for Decompress Run-Length Encoded List

--- leetcode_solutions/decompress-run-length-encoded-list.py
+++ leetcode_solutions/decompress-run-length-encoded-list.py
@@ -6,7 +6,5 @@ class Solution:
             if  i%2==0 and i+1 < len(nums):
                 for j in range(n):
                     result.append(nums[i+1])
-            else:
-                i+=1
         return result
             
