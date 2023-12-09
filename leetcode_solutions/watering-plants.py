e944a32fc269aeb1fac119b948d5f47ba2328921
Medina Nesro <mediunique00@gmail.com>
2023-12-09 22:53:54 +0300

    Added solution for Watering Plants

---
+++ leetcode_solutions/watering-plants.py
@@ -0,0 +1,12 @@
+class Solution:
+    def wateringPlants(self, plants: List[int], capacity: int) -> int:
+        steps=0
+        current_capacity=capacity
+        for i in range(len(plants)):
+            if current_capacity<plants[i]:
+                steps += i*2
+                current_capacity=capacity
+            steps+=1
+            current_capacity=current_capacity-plants[i]
+
+        return steps
