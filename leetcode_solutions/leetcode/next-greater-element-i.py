52d1eb659013653774fa1a79e29062ace1810251
Medina Nesro <mediunique00@gmail.com>
2024-02-24 06:28:59 +0300

    Add solution for Next Greater Element I

---
+++ leetcode_solutions/leetcode/next-greater-element-i.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        ind={n:i for i,n in enumerate(nums1)}
+        stack=[]
+        res=[-1]*len(nums1)
+        for i in range(len(nums2)):
+            while stack and nums2[i]>stack[-1]:
+                val=stack.pop()
+                j=ind[val]
+                
+                res[j]=nums2[i]
+            if nums2[i] in ind:
+                stack.append(nums2[i])
+        return res
