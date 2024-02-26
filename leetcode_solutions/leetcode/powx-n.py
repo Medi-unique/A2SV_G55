fbebe799b65c99a8d84016993eeee0e45ba60387
Medina Nesro <mediunique00@gmail.com>
2024-02-26 20:03:03 +0300

    Add solution for Pow(x, n)

---
+++ leetcode_solutions/leetcode/powx-n.py
@@ -0,0 +1,19 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        @cache
+        def power(n):
+            
+            
+            if n==1:
+                return x
+            if n==0:
+                return 1
+
+            
+            res = power(n-n//2) * power(n//2)
+                
+            return res
+        return power(n) if n>=0 else 1/power(abs(n))
+            
+
+        
